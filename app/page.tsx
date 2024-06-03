247bc544fe0120fa4d317f97b6e3da0da2a774b9
Shaon <shaon.softbdltd@gmail.com>
2024-06-03 14:50:10 +0600

    feat: added mantine ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx test ets</code>
-        </p>
-      </div>
+      <div>Boiler Plate</div>
     </main>
   );
 }
